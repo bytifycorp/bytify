b210db6ab66e15d6b744cf2c855a4b771059cdb2
Bytify <bytifycorp@gmail.com>
2024-10-29 19:32:45 +0530

    updated analytics
    updated analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import HeaderNav from "@/components/custom/header-nav";
 import FooterPromotion from "@/components/custom/footer-promotion";
 import Footer from "@/components/custom/footer";
 import { webConfig } from "@/config";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -46,13 +46,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <GoogleTagManager gtmId="G-XKX4ZYC6JL" />
             <body className={inter.className}>
                 <div className="relative">
                     <HeaderNav />
                     {children}
                     {/* <FooterPromotion /> */}
                     <Footer />
-                    <GoogleAnalytics gaId="G-XKX4ZYC6JL" />
                 </div>
             </body>
         </html>
